004f17b7d0a3ea7e9926d5be627e62df5af99b2d
Jakub Figlak <jakub.figlak@gmail.com>
2020-05-07 21:24:13 +0200

    Create scripts to load and delete data from the database

---
+++ seeder.js
@@ -0,0 +1,53 @@
+const fs = require('fs');
+const mongoose = require('mongoose');
+const colors = require('colors');
+const dotenv = require('dotenv');
+
+// Load env variables
+dotenv.config({ path: './config/config.env' });
+
+// Load models
+const Bootcamp = require('./models/Bootcamp');
+
+// Connect to DB
+mongoose.connect(process.env.DATABASE, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true,
+});
+
+// Read JSON files
+const bootcamps = JSON.parse(
+  fs.readFileSync(`${__dirname}/_data/bootcamps.json`, 'utf-8')
+);
+
+// Import into DB
+const importData = async () => {
+  try {
+    await Bootcamp.create(bootcamps);
+
+    console.log('Data Imported 💚💚💚...'.green.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+// Delete data
+const deleteData = async () => {
+  try {
+    await Bootcamp.deleteMany();
+
+    console.log('Data Destroyed 💣💣💣...'.red.inverse);
+    process.exit();
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+if (process.argv[2] === '-import') {
+  importData();
+} else if (process.argv[2] === '-destroy') {
+  deleteData();
+}
