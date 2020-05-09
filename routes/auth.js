70caadadcab1327117fdcc04d937ef03b5b31dbd
Jakub Figlak <jakub.figlak@gmail.com>
2020-05-09 22:02:51 +0200

    Create update password route

--- routes/auth.js
+++ routes/auth.js
@@ -7,6 +7,8 @@ const {
   getMe,
   forgotPassword,
   resetPassword,
+  updateDetails,
+  updatePassword,
 } = require('../controllers/authController');
 
 const router = express.Router();
@@ -14,7 +16,17 @@ const router = express.Router();
 router.post('/register', asyncHandler(register));
 router.post('/login', asyncHandler(login));
 router.get('/me', asyncHandler(protect), asyncHandler(getMe));
+router.put(
+  '/updatedetails',
+  asyncHandler(protect),
+  asyncHandler(updateDetails)
+);
 router.post('/forgotpassword', asyncHandler(forgotPassword));
 router.put('/resetpassword/:resettoken', asyncHandler(resetPassword));
+router.put(
+  '/updatepassword',
+  asyncHandler(protect),
+  asyncHandler(updatePassword)
+);
 
 module.exports = router;
