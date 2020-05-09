24203d8107155eb0c6aab32b02e4d46419b6d498
Jakub Figlak <jakub.figlak@gmail.com>
2020-05-09 21:45:41 +0200

    Finish password reset funcionality

--- routes/auth.js
+++ routes/auth.js
@@ -1,12 +1,20 @@
 const express = require('express');
 const asyncHandler = require('../middleware/async');
 const { protect } = require('../middleware/auth');
-const { register, login, getMe } = require('../controllers/authController');
+const {
+  register,
+  login,
+  getMe,
+  forgotPassword,
+  resetPassword,
+} = require('../controllers/authController');
 
 const router = express.Router();
 
 router.post('/register', asyncHandler(register));
 router.post('/login', asyncHandler(login));
 router.get('/me', asyncHandler(protect), asyncHandler(getMe));
+router.post('/forgotpassword', asyncHandler(forgotPassword));
+router.put('/resetpassword/:resettoken', asyncHandler(resetPassword));
 
 module.exports = router;
